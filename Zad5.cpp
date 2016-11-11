14d69c3d11ffaab96ee38e0dab89462857e7b730
skaltso12 <swawi_vs@abv.bg>
2016-11-11 17:16:33 +0200

    Update Zad5.cpp

--- Zad5.cpp
+++ Zad5.cpp
@@ -5,12 +5,18 @@ using namespace std;
 int main()
 {
 	double x, y, u, v;
-	cin >> x >> y >> u >> v;
+	cin >> x >> y;
+	cin >> u >> v;
 	x += u;
 	y += v;
-	if (x*x + y*y <= 1) cout << "60\n";
-	else if (x*x + y*y <= 9) cout << "20\n";
-	else if (x*x + y*y <= 64) cout << "10\n";
-	else cout << "0\n";
+	if (abs(x*x + y*y - 1) < 1.e-3 || abs(x*x + y*y - 9) < 1.e-3 || abs(x*x + y*y - 64) < 1.e-3) {
+		cout << "0\n";
+		return 0;
+	}
+	if (x*x + y*y < 1) cout << "60";
+	else if (x*x + y*y < 9) cout << "20";
+	else if (x*x + y*y < 64) cout << "10";
+	else cout << "0";
+	cout << "\n";
 	return 0;
 }
