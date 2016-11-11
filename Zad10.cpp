4d2864bd1db86d34940cfcd8ae4e592a50c019c1
skaltso12 <swawi_vs@abv.bg>
2016-11-11 15:44:45 +0200

    Update Zad10.cpp

--- Zad10.cpp
+++ Zad10.cpp
@@ -1,4 +1,3 @@
-// Zad10.cpp
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -9,16 +8,18 @@ int main()
 	cin >> a11 >> a12 >> b1;
 	cin >> a21 >> a22 >> b2;
 	det = a11 * a22 - a21 * a12;
-	if (det != 0) {
+	if (abs(det) >= 1.e-5) {
 		double det1, det2;
 		det1 = a22 * b1 - a12 * b2;
 		det2 = a11 * b2 - a21 * b1;
-		cout << setw(5) << (det1 / det) << " " << setw(5) << (det2 / det);
+		cout.precision(5);
+		cout << (det1 / det) << " " << (det2 / det);
 	}
 	else if (abs(a11) < 1.e-5) {
 		if (abs(a12) < 1.e-5)
 			if (abs(b1) < 1.e-5) cout << "Many solutions"; else cout << "No solution";
-		else if (abs(a12 - a21) < 1.e-5 && abs(b1 - b2) < 1.e-5) cout << "Many solutions";
+		else if (abs(a12 - a21) < 1.e-5 && abs(b1 - b2) < 1.e-5 || abs(a22) < 1.e-5 
+			&& abs(b2) < 1.e-5 || abs(b2) < 1.e-5 && abs(b1) < 1.e-5) cout << "Many solutions";
 		else cout << "No solution";
 	}
 	else {
